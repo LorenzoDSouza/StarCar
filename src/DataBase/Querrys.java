f47bec2f5b21aeacb1b066b3cad6d7b0ee86426c
Lorenzo <lkirstdesouza@gmail.com>
2024-03-18 11:01:47 -0300

    Started to create the methods on the class Querry

--- src/DataBase/Querrys.java
+++ src/DataBase/Querrys.java
@@ -1,5 +1,26 @@
 package DataBase;
 
-public class Querrys {
+import java.util.*;
+import java.sql.*;
 
+public interface Querrys {
+	
+	public static int selectInt(String table, String column, int index, String id, Statement stmt) {
+		int result;
+		String q = "SELECT " + column + " FROM " + table + " WHERE " + id + " = " + index;
+		
+		try {
+			ResultSet rs = stmt.executeQuery(q);
+			result = rs.getInt(column);
+			
+			
+		} catch (SQLException e) {
+			System.out.println("Couldn't execute querry! The value position is invalid or the datatype missmatch.");
+			result = 0;
+		}
+		
+		
+		return result;
+	}
+	
 }
