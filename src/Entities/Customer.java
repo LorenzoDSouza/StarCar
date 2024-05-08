681065d2ae82c4fc9b38ba89dd1265b323c7015b
Lorenzo Souza <lkirstdesouza@gmail.com>
2024-05-08 13:20:55 -0300

    Create a method to get all the customers from the database and add to a arraylist in the api(customersRepository)

--- src/Entities/Customer.java
+++ src/Entities/Customer.java
@@ -4,41 +4,48 @@ import java.util.*;
 
 public class Customer {
 
-	private String name;
+	private String first_name;
+	private String last_name;
 	private int customer_id;
 
-	public Customer(int customer_id, String name) {
+	public Customer(int customer_id, String first_name, String last_name) {
 		this.customer_id = customer_id;
-		this.name = name;
+		this.first_name = first_name;
+		this.last_name = last_name;
 	}
 	
-	public Customer (String name) {
-		this.name = name;
+	public Customer (String first_name, String last_name) {
+		this.first_name = first_name;
+		this.last_name = last_name;
 	}
 	
-	public static Customer create(int customer_id, String name) {
-		return new Customer(customer_id, name);
+	public static Customer create(int customer_id, String first_name, String last_name) {
+		return new Customer(customer_id, first_name, last_name);
 	}
 	
-	public static Customer create (String name) {
-		return new Customer(name);
+	public static Customer create (String first_name, String last_name) {
+		return new Customer(first_name, last_name);
 	}
-	
-	public void setName(String name) {
-		this.name = name;
+
+
+	public String getFirst_name() {
+		return first_name;
+	}
+
+	public void setFirst_name(String first_name) {
+		this.first_name = first_name;
+	}
+
+	public String getLast_name() {
+		return last_name;
+	}
+
+	public void setLast_name(String last_name) {
+		this.last_name = last_name;
 	}
 
 	public void setCustomer_id(int customer_id) {
 		this.customer_id = customer_id;
-	}
-
-	public String toString() {
-		return "Name: " + name + " | Customer Id: " + customer_id;
-		
-	}
-
-	public String getName() {
-		return name;
 	}
 
 	public int getCustomer_id() {
--- src/Repositories/CustomerRepository.java
+++ src/Repositories/CustomerRepository.java
@@ -13,7 +13,19 @@ public class CustomerRepository {
 	private DbConnection connection;
 	private ArrayList<Customer> customers;
 	
-	
+	public Customer createCustomerLogic(ResultSet resultSet) {
+
+		try {
+			int customer_id = resultSet.getInt("customer_id");
+			String customer_name = resultSet.getString("first_name");
+			
+			Customer customer = Customer.create(customer_id, customer_name);
+
+			return customer;
+		} catch (Exception e) {
+			return null;
+		}
+	}
 
 	
 	
