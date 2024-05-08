708a8cf54cfe7dcd70225f80119700dd70624e17
Lorenzo Souza <lkirstdesouza@gmail.com>
2024-05-08 12:46:20 -0300

    remakes the Customer (Entities) class so it fits in the new refactoring.

--- src/Entities/Customer.java
+++ src/Entities/Customer.java
@@ -11,6 +11,31 @@ public class Customer {
 		this.customer_id = customer_id;
 		this.name = name;
 	}
+	
+	public Customer (String name) {
+		this.name = name;
+	}
+	
+	public static Customer create(int customer_id, String name) {
+		return new Customer(customer_id, name);
+	}
+	
+	public static Customer create (String name) {
+		return new Customer(name);
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setCustomer_id(int customer_id) {
+		this.customer_id = customer_id;
+	}
+
+	public String toString() {
+		return "Name: " + name + " | Customer Id: " + customer_id;
+		
+	}
 
 	public String getName() {
 		return name;
