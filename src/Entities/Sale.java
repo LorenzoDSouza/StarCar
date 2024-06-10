607d51cd42d52c6760eccef902d70a4374702d50
Lorenzo Souza <lkirstdesouza@gmail.com>
2024-06-10 12:15:39 -0300

    Create the gets and sets int he Sale entitie class

--- src/Entities/Sale.java
+++ src/Entities/Sale.java
@@ -27,5 +27,39 @@ public class Sale {
 	public static Sale create(int car_id, int salesPerson_id, int customer_id) {
 		return new Sale(car_id, salesPerson_id, customer_id);
 	}
+
+	public int getSale_id() {
+		return sale_id;
+	}
+
+	public void setSale_id(int sale_id) {
+		this.sale_id = sale_id;
+	}
+
+	public int getCar_id() {
+		return car_id;
+	}
+
+	public void setCar_id(int car_id) {
+		this.car_id = car_id;
+	}
+
+	public int getSalesPerson_id() {
+		return salesPerson_id;
+	}
+
+	public void setSalesPerson_id(int salesPerson_id) {
+		this.salesPerson_id = salesPerson_id;
+	}
+
+	public int getCustomer_id() {
+		return customer_id;
+	}
+
+	public void setCustomer_id(int customer_id) {
+		this.customer_id = customer_id;
+	}
+	
+	
 	
 }
