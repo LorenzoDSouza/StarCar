927734f495033174ff077ff96e4e7b0dbb8213d2
Lorenzo Souza <lkirstdesouza@gmail.com>
2024-06-10 12:13:12 -0300

    Refactor the Sale entitie class

--- src/Entities/Sale.java
+++ src/Entities/Sale.java
@@ -2,9 +2,30 @@ package Entities;
 
 public class Sale {
 
-	private int sale_Id;
-	private SalesPerson salePerson;
-	private Customer customer;
-	private Car car;
-
+	private int sale_id;
+	private int car_id;
+	private int salesPerson_id;
+	private int customer_id;
+	
+	public Sale(int sale_id, int car_id, int salesPerson_id, int customer_id) {
+		this.sale_id = sale_id;
+		this.car_id = car_id;
+		this.salesPerson_id = salesPerson_id;
+		this.customer_id = customer_id;
+	}
+	
+	public Sale(int car_id, int salesPerson_id, int customer_id) {
+		this.car_id = car_id;
+		this.salesPerson_id = salesPerson_id;
+		this.customer_id = customer_id;
+	}
+	
+	public static Sale create(int sale_id, int car_id, int salesPerson_id, int customer_id) {
+		return new Sale(sale_id, car_id, salesPerson_id, customer_id);
+	}
+	
+	public static Sale create(int car_id, int salesPerson_id, int customer_id) {
+		return new Sale(car_id, salesPerson_id, customer_id);
+	}
+	
 }
