3d808a24a4a2956a8e5efac6bd6a97888c92dd61
Lorenzo Souza <lkirstdesouza@gmail.com>
2024-06-11 16:19:10 -0300

    Create the getSales method in the SalesRepository class

--- src/Repositories/SalesRepository.java
+++ src/Repositories/SalesRepository.java
@@ -7,14 +7,14 @@ import java.util.ArrayList;
 import Database.DbConnection;
 import Repositories.*;
 import Entities.*;
+import Exceptions.AppException;
 
 public class SalesRepository {
-	
+
 	private DbConnection connection;
 	private ArrayList<Sale> sales;
-	
-	// getAllSales
-	// register (need to have check if the car ialready related to a sale\
+	// getAllSales -
+	// register (need to have check if the car is already related to a sale)
 	// getById
 	// deleteById
 	// getLastSale
@@ -24,48 +24,69 @@ public class SalesRepository {
 	// updateSalesPersonId
 	// updateCustomerId
 	// updateCarId
-	
+
 	public SalesRepository(String database) {
 		connection = new DbConnection(database);
-		//sales = getAllSales();
-		}
-	
+		sales = getAllSales();
+	}
+
 	public Sale createSaleLogic(ResultSet resultSet) {
-		
+
 		try {
 			int sale_id = resultSet.getInt("sale_id");
 			int car_id = resultSet.getInt("car_id");
 			int salesPerson_id = resultSet.getInt("salesPerson_id");
 			int customer_id = resultSet.getInt("customer_id");
-			
+
 			Sale sale = Sale.create(sale_id, car_id, salesPerson_id, customer_id);
-			
+
 			return sale;
-		} catch(Exception e) {
+		} catch (Exception e) {
 			System.out.println("Couldnt create the Sale logic: " + e.getMessage());
 			return null;
 		}
 	}
-	
-	public ArrayList<Sale> getAllSales(){
+
+	public ArrayList<Sale> getAllSales() {
 		ArrayList<Sale> sales = new ArrayList<Sale>();
 		String querrySelect = "SELECT * FROM sale";
-		
+
 		try {
 			ResultSet resultSet = connection.statement.executeQuery(querrySelect);
-			
-			while(resultSet.next()) {
+
+			while (resultSet.next()) {
 				Sale sale = createSaleLogic(resultSet);
 				sales.add(sale);
 			}
 			return sales;
-		}  catch (SQLException e) {
+		} catch (SQLException e) {
 			System.out.println("Couldn't execute querry: " + e.getMessage());
 			return null;
 		} catch (RuntimeException e) {
 			System.out.println("There was a problem to get all the sales person: " + e.getMessage());
 			return null;
 		}
+	}
+	
+	public ArrayList<Sale> getSales(){
+		return sales;
+	}
+
+	public Sale register(Sale sale, CarsRepository carsRep) {
+		int car_id = sale.getCar_id();
+		boolean isCarSold = carsRep.getById(car_id).getSoldValue();
+		
+		String 
+		
+		try {
+		if(isCarSold == true) {
+			throw new AppException("The car is already sold!");
+			
+			}
+		
+		
+		}
+		
 	}
 
 }
