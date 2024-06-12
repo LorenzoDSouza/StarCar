52684bd7d5561e3c1ba3e2a9f487e3bf05a184af
Lorenzo Souza <lkirstdesouza@gmail.com>
2024-06-12 15:36:13 -0300

    Create the getLastSale method and finish the register one in the SalesRepository class

--- src/Repositories/SalesRepository.java
+++ src/Repositories/SalesRepository.java
@@ -67,26 +67,62 @@ public class SalesRepository {
 			return null;
 		}
 	}
-	
-	public ArrayList<Sale> getSales(){
+
+	public ArrayList<Sale> getSales() {
 		return sales;
 	}
 
-	public Sale register(Sale sale, CarsRepository carsRep) {
-		int car_id = sale.getCar_id();
+	public Sale register(Sale saleParam, CarsRepository carsRep) {
+		int car_id = saleParam.getCar_id();
 		boolean isCarSold = carsRep.getById(car_id).getSoldValue();
 		
-		String insertValueQuerry = "INSERT INTO sale ()"
+		String insertValueQuerry = "INSERT INTO sale (car_id, salesPerson_id, customer_id) VALUES (" + saleParam.getCar_id() + ", " + saleParam.getSalesPerson_id() + ", " + saleParam.getCustomer_id() + ")"; 
 		
 		try {
 		if(isCarSold == true) {
 			throw new AppException("The car is already sold!");
-			
 			}
 		
-		
+			connection.statement.executeUpdate(insertValueQuerry);
+			
+			Sale sale = getLastSale();
+			return sale;
+		} catch (SQLException e) {
+			System.out.println("There was a problem to execute the querry (SQLException): " + e.getMessage());
+			return null;
+		} catch (RuntimeException e) {
+			System.out
+					.println("There was a problem to register the sale (RuntimeException): " + e.getMessage());
+			return null;
+		} catch (AppException e) {
+			System.out.println("There was a problem to register the sale (AppException): " + e.getMessage());
+			return null;
 		}
 		
 	}
 
-}
+	public Sale getLastSale() {
+		String getLastSaleQuerry = "SELECT * FROM sale ORDER BY sale_id DESC LIMIT 1;";
+
+		try {
+			ResultSet resultSet = connection.statement.executeQuery(getLastSaleQuerry);
+
+			if (resultSet.next()) {
+				Sale sale = createSaleLogic(resultSet);
+				return sale;
+			} else {
+				throw new AppException("Couldnt execute the querry. None row was returned");
+			}
+
+		} catch (SQLException e) {
+			System.out.println("There was a problem to get the last sales (SQLException): " + e.getMessage());
+			return null;
+		} catch (RuntimeException e) {
+			System.out.println("There was a problem to get the last sales (RuntimeException): " + e.getMessage());
+			return null;
+		} catch (AppException e) {
+			System.out.println("There was a problem to get the last sales (AppException): " + e.getMessage());
+			return null;
+		}
+	}
+}}
